1eed320b3952faeb7293a1f54019db1ab1488d91
MCollinaWS <m.collina@whitesock.it>
2025-10-28 15:08:47 +0100

    chore: updated get names chmod and commetns

--- .github/script/get-names.sh
+++ .github/script/get-names.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# IF THE WORKFLOW FAILS BECAUSE OF PERMISSION USE THIS COMMAND IN GIT SHELL: 'git update-index --chmod=+x ./.github/script/get-project-name.sh'
+# IF THE WORKFLOW FAILS BECAUSE OF PERMISSION USE THIS COMMAND IN GIT SHELL: 'git update-index --chmod=+x ./.github/script/get-names.sh'
 # REPO_NAME contains the github.repository value which is owner/repo-name.
 echo "${REPO_NAME}"
 # Split by / and taking the element at index 1 (owner/repo-name -> repo-name)
 readarray -d "/" -t splitted <<< "${REPO_NAME}"  
 repoName="${splitted[1]}"
 # By guidelines, repo name are written with a dash separator between context and underscore to mark spaces. 
-# I.E. client_name-very_long_repo_name-another_context -> This should return VeryLonRepoName.
+# I.E. client_name-very_long_repo_name-another_context
 # So start by trying splitting the repo-name by dash (-)
 readarray -d "-" -t splitted <<< "$repoName"
 # Then get the element at index 1 (if it exists) otherwise take the 0.
@@ -19,6 +19,7 @@ echo $projectName
 # This output is then used inside the job 'outputs' to register a variable.
 echo "name=${projectName}" >> $GITHUB_OUTPUT 
 
+# If the taken value is at index 1, it means client name is available at index 0
 if [[ "$min" == 1 ]]; then
     readarray -d "-" -t splitted <<< "$repoName"
     clientName="${splitted[0]}"
